c4c90d397e917dd056f052769530c361b1ecafb2
adriaclavero2 <adriaclavero2@gmail.com>
2026-03-31 09:41:54 +0200

    Delete fruit-api-h2 directory

--- fruit-api-h2/src/main/java/cat/itacademy/s04/t02/n01/fruit/exception/FruitNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package cat.itacademy.s04.t02.n01.fruit.exception;
-
-public class FruitNotFoundException extends RuntimeException {
-    public FruitNotFoundException(String message) {
-        super(message);
-    }
-}
